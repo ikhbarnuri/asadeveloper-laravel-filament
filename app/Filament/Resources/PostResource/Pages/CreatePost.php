05a2f00b21cf50be7c5550a684d749a8ff68f726
Ikhbar Nurinsani <ikhbarnuri@gmail.com>
2023-12-14 11:37:36 +0700

    menambah database notification

--- app/Filament/Resources/PostResource/Pages/CreatePost.php
+++ app/Filament/Resources/PostResource/Pages/CreatePost.php
@@ -3,10 +3,25 @@
 namespace App\Filament\Resources\PostResource\Pages;
 
 use App\Filament\Resources\PostResource;
+use App\Models\User;
+use Filament\Notifications\Notification;
 use Filament\Pages\Actions;
 use Filament\Resources\Pages\CreateRecord;
+use Illuminate\Support\Facades\Auth;
 
 class CreatePost extends CreateRecord
 {
     protected static string $resource = PostResource::class;
+
+    protected function getRedirectUrl(): string
+    {
+        $name = Auth::user()->name;
+        Notification::make()
+            ->success()
+            ->title('Post Created By ' . $name)
+            ->body('New Post Has Been Saved')
+            ->sendToDatabase(User::query()->whereNot('id', \auth()->user()->id)->get());
+
+        return $this->previousUrl ?? $this->getResource()::getUrl('index');
+    }
 }
