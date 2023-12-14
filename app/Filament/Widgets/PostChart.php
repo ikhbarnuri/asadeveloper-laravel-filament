9e1c0ff4cdb5b387940960ff16d55e1e11714fee
Ikhbar Nurinsani <ikhbarnuri@gmail.com>
2023-12-14 10:40:14 +0700

    menambah chart widget

---
+++ app/Filament/Widgets/PostChart.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use App\Models\Post;
+use Carbon\Carbon;
+use Filament\Widgets\LineChartWidget;
+use Flowframe\Trend\Trend;
+use Flowframe\Trend\TrendValue;
+
+class PostChart extends LineChartWidget
+{
+    protected static ?string $heading = 'Posts Chart';
+
+    protected function getData(): array
+    {
+        $data = Trend::model(Post::class)
+            ->between(
+                start: now()->startOfYear(),
+                end: now()->endOfYear(),
+            )
+            ->perMonth()
+            ->count();
+
+        return [
+            'datasets' => [
+                [
+                    'label' => 'Blog posts',
+                    'data' => $data->map(fn(TrendValue $value) => $value->aggregate),
+                ],
+            ],
+            'labels' => $data->map(function (TrendValue $value) {
+                $date = Carbon::createFromFormat('Y-m', $value->date);
+                return $date->format('M');
+            }),
+        ];
+    }
+}
